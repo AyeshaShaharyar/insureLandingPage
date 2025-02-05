a9ccd48aab15f99e45af59f5683c93f0df37a32c
AyeshaShaharyar <ayesha2598@gmail.com>
2025-02-04 23:24:40 -0500

    add nav bar and custom themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        darkViolet: "hsl(256, 26%, 20%)",
+        grayishBlue: "hsl(216, 30%, 68%)",
+        veryDarkViolet: "hsl(270, 9%, 17%)",
+        darkGrayishViolet: "hsl(273, 4%, 51%)",
+        veryLightGray: "hsl(0, 0%, 98%)",
+      },
+    },
   },
   plugins: [],
 };
